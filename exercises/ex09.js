44256662a5c022f9efbb495eae674a5afedc113f
Sae Nakatani <nakatanisae0218@gmail.com>
2026-05-21 17:45:09 -0700

    Exercise 9 completed

--- exercises/ex09.js
+++ exercises/ex09.js
@@ -1,5 +1,6 @@
 /*
-NASA's countdown to launch includes checkpoints where NASA engineers complete certain technical tasks. During the final minute, NASA has 6 tasks to complete:
+NASA's countdown to launch includes checkpoints where NASA engineers complete certain technical tasks. 
+During the final minute, NASA has 6 tasks to complete:
 
 Orbiter transfers from ground to internal power (T-50 seconds)
 Ground launch sequencer is go for auto sequence start (T-31 seconds)
@@ -31,3 +32,23 @@ T-2 seconds
 T-1 seconds
 Solid rocket booster ignition and liftoff!
 */
+
+let countdown = 60
+while(countdown >= 0){
+    if(countdown === 50){
+        console.log("Orbiter transfers from ground to internal power")
+    } else if (countdown === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start")
+    } else if (countdown === 16){
+        console.log("Activate launch pad sound suppression system")
+    } else if (countdown === 10){
+        console.log("Activate main engine hydrogen burnoff system ")
+    } else if (countdown === 6){
+        console.log("Main engine start ")
+    } else if (countdown === 0){
+        console.log("Solid rocket booster ignition and liftoff!")
+    } else {
+        console.log(`T-${countdown} seconds`)
+    }
+    countdown--;   
+}
